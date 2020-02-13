6dcbe0e664105e48dd0f51c9ec67a6b73ff78380
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-13 20:32:08 -0300

    JPA repository, database seeding

---
+++ src/main/java/com/lucasmourao/baobhapi/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.lucasmourao.baobhapi.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucasmourao.baobhapi.entities.Place;
+import com.lucasmourao.baobhapi.repositories.PlaceRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private PlaceRepository placeRepository;
+	
+	@Override
+	public void run(String... agrs) throws Exception{
+		String [] businessHours = {"Segunda: 8:00 às 17:00","Terça: 8:00 às 17:00","Quarta: 8:00 às 17:00",
+				"Quinta: 8:00 às 17:00","Sexta: 8:00 às 17:00","Sabádo: 8:00 às 17:00",
+				"Domingo: 8:00 às 17:00"};
+		Place place1 = new Place(null, "Zoologico", "Lugar para ver animais", "Rua Otacilio Negrão de Lima", businessHours, 4.5);
+		Place place2 = new Place(null, "Parque Ecologico", "Contato com a natureza", "Rua Otacilio Negrão de Lima", businessHours, 4.7);
+		
+		placeRepository.saveAll(Arrays.asList(place1,place2));
+	}
+}
---
+++ src/main/java/com/lucasmourao/baobhapi/repositories/PlaceRepository.java
@@ -0,0 +1,9 @@
+package com.lucasmourao.baobhapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasmourao.baobhapi.entities.Place;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+		
+}
