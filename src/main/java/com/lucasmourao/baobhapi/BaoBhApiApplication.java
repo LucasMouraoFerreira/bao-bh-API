3002bce6ddb95acbff5ea81e668e8a4af12a48cb
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-13 18:04:56 -0300

    Project Created

---
+++ src/main/java/com/lucasmourao/baobhapi/BaoBhApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmourao.baobhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoBhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoBhApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmourao/baobhapi/BaoBhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmourao.baobhapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoBhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
