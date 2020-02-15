02ce01c59f5c65f8a86b6a624c2d1c91f8a688c7
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-15 12:58:04 -0300

    Places delete exceptions handler

--- src/main/java/com/lucasmourao/baobhapi/services/PlaceService.java
+++ src/main/java/com/lucasmourao/baobhapi/services/PlaceService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lucasmourao.baobhapi.entities.Place;
 import com.lucasmourao.baobhapi.repositories.PlaceRepository;
+import com.lucasmourao.baobhapi.services.exceptions.DatabaseException;
 import com.lucasmourao.baobhapi.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class PlaceService {
 	}
 
 	public void delete(Long id) {
-		placeRepository.deleteById(id);
+		try {
+			placeRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}		
 	}
 
 	public Place update(Long id, Place place) {
