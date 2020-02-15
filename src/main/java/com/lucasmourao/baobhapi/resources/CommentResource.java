fc4847bda420bf22a0751b9d4dd7db7ca1ead404
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-15 11:57:09 -0300

    Comment post method

--- src/main/java/com/lucasmourao/baobhapi/resources/CommentResource.java
+++ src/main/java/com/lucasmourao/baobhapi/resources/CommentResource.java
@@ -1,5 +1,6 @@
 package com.lucasmourao.baobhapi.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,11 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasmourao.baobhapi.dto.CommentDTO;
-import com.lucasmourao.baobhapi.services.CommentService;;
+import com.lucasmourao.baobhapi.entities.Comment;
+import com.lucasmourao.baobhapi.entities.Place;
+import com.lucasmourao.baobhapi.services.CommentService;
+import com.lucasmourao.baobhapi.services.PlaceService;;
 
 @RestController
 @RequestMapping(value = "/comments")
@@ -19,6 +26,9 @@ public class CommentResource {
 
 	@Autowired
 	private CommentService commentService;
+	
+	@Autowired
+	private PlaceService placeService;
 	
 	@GetMapping
 	public ResponseEntity<List<CommentDTO>> findAll(){
@@ -30,5 +40,13 @@ public class CommentResource {
 	public ResponseEntity<CommentDTO> findById(@PathVariable Long id){
 		CommentDTO comment = new CommentDTO(commentService.findById(id));
 		return ResponseEntity.ok().body(comment);
+	}
+	
+	@PostMapping(value="/{place_id}")
+	public ResponseEntity<CommentDTO> insert(@PathVariable Long place_id, @RequestBody Comment comment){
+		Place place = placeService.findById(place_id);
+		comment = commentService.insert(comment, place);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(comment.getId()).toUri();
+		return ResponseEntity.created(uri).body(new CommentDTO(comment));
 	}
 }
--- src/main/java/com/lucasmourao/baobhapi/services/CommentService.java
+++ src/main/java/com/lucasmourao/baobhapi/services/CommentService.java
@@ -1,5 +1,6 @@
 package com.lucasmourao.baobhapi.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmourao.baobhapi.entities.Comment;
+import com.lucasmourao.baobhapi.entities.Place;
 import com.lucasmourao.baobhapi.repositories.CommentRepository;
 
 @Service
@@ -24,4 +26,8 @@ public class CommentService {
 		return comment.get(); 
 	}
 	
+	public Comment insert(Comment comment, Place place) {
+		Comment aux = new Comment(null, Instant.now(),comment.getText(),comment.getAuthor(),place);
+		return commentRepository.save(aux);
+	}
 }
